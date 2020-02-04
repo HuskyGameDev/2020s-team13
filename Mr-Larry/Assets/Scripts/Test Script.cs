025bbc57e659e3a56c2b5a45824a00d0b2c297a8
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2020-02-04 16:30:31 -0500

    initialize github

---
+++ Mr-Larry/Assets/Scripts/Test Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    int Hello(int x, int y)
+    {
+        return x + y;  
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
