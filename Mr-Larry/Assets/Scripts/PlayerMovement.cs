b433a679e1ac4bd77c87acf3a5cc2278ee861663
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2020-03-17 16:31:53 -0400

    Create PlayerMovement.cs

---
+++ Mr-Larry/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float xMovement=500f;
+    public float yMovement = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            {
+            rb.AddForce(0, yMovement * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, -yMovement * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-xMovement * Time.deltaTime, 0,0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(xMovement * Time.deltaTime, 0,0);
+        }
+    }
+}
