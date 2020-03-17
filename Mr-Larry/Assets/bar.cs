9356844fe5088d7a7efdf0413fdc03d87b7bc04f
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2020-03-17 16:47:53 -0400

    Add files via upload

---
+++ Mr-Larry/Assets/bar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bar : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float yMovement = 500f;
+    public float gravity = 150f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            rb.AddForce(0, yMovement * Time.deltaTime, 0);
+        }
+        rb.AddForce(0, -gravity* Time.deltaTime, 0);
+    }
+}
