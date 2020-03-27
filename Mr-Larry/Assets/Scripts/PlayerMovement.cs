52d558e0a91b5a3b3bf40aff78e8804435932003
Dominic Boggio <dominicboggio@gmail.com>
2020-03-26 21:57:16 -0400

    Convert everything to 2D, and add a Fish Eating system.
    All Rigidbodys are Rigidbody2D's, (meaning the movement system is now changed), and now Larry can have some dinner.

--- Mr-Larry/Assets/Scripts/PlayerMovement.cs
+++ Mr-Larry/Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float xMovement=500f;
-    public float yMovement = 500f;
+    public Rigidbody2D rb;
+    public Vector2 velocity;
+    public float xMovement = 20f;
+    public float yMovement = 20f;
+    public SpriteRenderer spr;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        float acceleration = xMovement;
+        float deceleration = acceleration;
+        float moveInputX = Input.GetAxisRaw("Horizontal");
+        float moveInputY = Input.GetAxisRaw("Vertical");
+        /*if (Input.GetKey("w"))
+        {
+            Debug.Log("w");
+            if(rb.velocity.y < 0)
             {
-            rb.AddForce(0, yMovement * Time.deltaTime, 0);
+                rb.velocity.Set(rb.velocity.x, 0f);
+            }
+            transform.position.Set(transform.position.x, transform.position.y + yMovement * Time.deltaTime, transform.position.z);
         }
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, -yMovement * Time.deltaTime, 0);
-        }
+            if (rb.velocity.y > 0)
+            {
+                rb.velocity.Set(rb.velocity.x, 0f);
+            }
+            transform.position.Set(transform.position.x, transform.position.y + yMovement * Time.deltaTime, transform.position.z);
+            //rb.AddForce(0, -yMovement * Time.deltaTime, 0);
+        }*/
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-xMovement * Time.deltaTime, 0,0);
+            spr.flipX = true;
         }
         if (Input.GetKey("d"))
         {
-            rb.AddForce(xMovement * Time.deltaTime, 0,0);
+            spr.flipX = false;
         }
+        if(moveInputX!=0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, xMovement * moveInputX, acceleration * Time.deltaTime);
+        }
+        else if(moveInputY!=0)
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, yMovement * moveInputY, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+            velocity.y = Mathf.MoveTowards(velocity.y, 0, deceleration * Time.deltaTime);
+        }
+        
+        transform.Translate(velocity * Time.deltaTime);
     }
 }
