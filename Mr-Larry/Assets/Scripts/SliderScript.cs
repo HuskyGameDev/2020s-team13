9dc1da2cd471575616f2ae801d7fdbb6fb532f8a
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2020-04-16 15:18:45 -0400

    Added the slider.
    Added the slider. Feel free to modify its power level (how high the value increases upon key press) during playtesting. By the way, the O key is what I have been using for the power bar to simulate arcade controls.

---
+++ Mr-Larry/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public Slider slider;
+    public int slide = 20;
+    public int power;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("o"))
+        {
+            slider.value = slider.value + slide;
+        }
+        if(slider.value < 25)
+        {
+            power = 1;
+            print(power);
+        }
+        else if (slider.value >25 && slider.value < 50)
+        {
+            power = 2;
+            print(power);
+        }
+        else if (slider.value > 50 && slider.value < 75)
+        {
+            power = 3;
+            print(power);
+        }
+        else if (slider.value > 75 && slider.value < 100)
+        {
+            power = 4;
+            print(power);
+        }
+        slider.value--;
+
+    }
+}
